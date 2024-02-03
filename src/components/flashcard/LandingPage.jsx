2e10423c9146e2bb4e774a4c81eab3f5e747494c
Kamil MARKOWSKI <kamilmarkowski@MacBook-Air-de-Kamil.local>
2024-02-03 10:58:09 +0100

    Landing page changed and recentered

--- src/components/flashcard/LandingPage.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function LandingPage() {
-    return <h1 className="greeting">Let's learn some new French words !</h1>
-}
--- src/hooks/supabase.js
+++
@@ -1,8 +0,0 @@
-import {useContext} from "react";
-import {SupabaseContext} from "../context/SupabaseContext.jsx";
-
-export const useSupabase = () => {
-    const context = useContext(SupabaseContext);
-    if (undefined === context) throw new Error('useSupabase must be used within a SupabaseProvider')
-    return context;
-};
