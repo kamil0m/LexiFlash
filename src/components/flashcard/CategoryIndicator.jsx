b9b72b511ac264a36c9717be6a5af14af14ebfd7
Kamil Markowski <kamil0markowski@gmail.com>
2024-02-08 15:17:30 +0100

    scss variables files names correcgted

--- src/components/flashcard/CategoryIndicator.jsx
+++
@@ -1,3 +0,0 @@
-export default function CategoryIndicator({currentFlashcard}) {
-    return <h1 className="set__title">{currentFlashcard.category}</h1>
-}
--- src/components/pages/LandingPage.jsx
+++
@@ -1,6 +0,0 @@
-export default function LandingPage() {
-    return <div className="intro">
-        <h1 className="app__title">LexiFlash</h1>
-        <h2 className="greeting">Let's learn some new French words !</h2>
-    </div>
-}
