a6348945ae746615b0412381abb8c4a56ce23031
Kamil Markowski <kamil0markowski@gmail.com>
2024-08-14 11:01:28 +0200

    header logo updated

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -10,7 +10,10 @@ export default function MainLayout() {
             <Navbar className="header">
                 <Container className="col-12 col-lg-8 mx-auto nav">
                     <Navbar.Brand>
-                        <NavLink to="/" className="logo" />
+                        <NavLink to="/" className="logo" >
+                            <i className="fa-solid fa-bolt logo__icon" />
+                            <span className="logo__name">LexiFlash</span>
+                        </NavLink>
                     </Navbar.Brand>
                     <ul className="navbar__links">
                         <NavLink className="navbar__element" to="/learn">learn</NavLink>
